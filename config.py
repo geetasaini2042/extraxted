f54057233909a5529a2676f643639e4e8f443708
geetasaini2042 <Geetasaini2042@gmail.com>
2025-10-17 15:42:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # (c) ACE 
-
+#api_id = 24673538
+#api_hash = "555639745e6ceee1ae3797866136998f"
 import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6216840269:AAFNzApyRlXuZ94T3UZgd-XqNun2mA40kJU")
-    API_ID = int(os.environ.get("API_ID", "7856100"))
-    API_HASH = os.environ.get("API_HASH", "dba409d7e8d44ab5bb689378deea1792")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8498552239:AAFQjLU6DePHb5sPvAFD7DR5sUCwsXdIFIc")
+    API_ID = int(os.environ.get("API_ID", "24673538"))
+    API_HASH = os.environ.get("API_HASH", "555639745e6ceee1ae3797866136998f")
     AUTH_USERS = "6150091802"
+
 
 
